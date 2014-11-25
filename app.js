8340a99fa57a2397cbb470ccd47cfe0a18246145
Soheil Rashidi <contact@soheilrashidi.com>
2014-11-26 00:40:30 +0330

    Initial commit.

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/', require('./routes/index'));
+
+app.use(function(request, response, next) {
+  var error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use(function(error, request, response, next) {
+  response.status(error.status || 500);
+  response.render('error', {
+    message: error.message,
+    error: app.get('env') === 'development' ? error : {}
+  });
+});
+
+module.exports = app;
