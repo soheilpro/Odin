4d9c5d03ad9f10005374ae69d15ddefd5ebeeebf
Soheil Rashidi <contact@soheilrashidi.com>
2014-11-30 00:43:52 +0330

    Added basic adding functionality.

--- assets/javascripts/app/main.js
+++ assets/javascripts/app/main.js
@@ -1,1 +1,11 @@
 var odinApp = angular.module('odinApp', []);
+
+odinApp.controller('ItemsController', ['$scope', function($scope) {
+  $scope.items = [];
+
+  $scope.addItem = function(title) {
+    this.items.push({
+      title: title,
+    });
+  };
+}])
